14eeac0651aa328d65b0ab60ff290055cbee17b4
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2018-01-25 20:05:07 -0600

    added content tests

--- src/components/feedback.test.js
+++ src/components/feedback.test.js
@@ -7,5 +7,13 @@ describe('<Feedback />', () => {
     it('Renders without crashing', () => {
         shallow(<Feedback />);
     });
+
+    it('Renders some feedback', () => {
+        let TEST_FEEDBACK = 'You are Warm';
+    
+        let wrapper = shallow(<Feedback feedback={TEST_FEEDBACK} />);
+        expect(wrapper.contains(TEST_FEEDBACK)).toEqual(true);
+    });
+
 });
 
